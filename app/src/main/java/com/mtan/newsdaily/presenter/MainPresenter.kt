88834ee84a8b27d11349a7d938e0227314c6aa06
dolpphins <1126924038@qq.com>
2020-07-04 17:31:33 +0800

    使用kotlin协程进行线程切换

--- app/src/main/java/com/mtan/newsdaily/presenter/MainPresenter.kt
+++ app/src/main/java/com/mtan/newsdaily/presenter/MainPresenter.kt
@@ -1,11 +1,10 @@
 package com.mtan.newsdaily.presenter
 
-import android.os.Handler
-import android.os.Looper
 import com.mtan.newsdaily.MainActivity
 import com.mtan.newsdaily.bean.News
 import com.mtan.newsdaily.core.Requester
 import com.mtan.newsdaily.util.TimeUtil
+import kotlinx.coroutines.*
 
 class MainPresenter(var activity: MainActivity) {
 
@@ -16,53 +15,55 @@ class MainPresenter(var activity: MainActivity) {
     private var mIsLoadingMore = false;
 
     fun loadFirst() {
-        if (mIsRefreshing) {
-            return
-        }
-        if (mIsLoadingMore) {
-            mIsLoadingMore = false
-        }
-        mIsRefreshing = true
-        mList.clear()
-        Thread{
-            val requester = Requester()
-            mNewsLastTime = System.currentTimeMillis()
-            val yesterdayList = requester.request(TimeUtil.getDayStr(mNewsLastTime - TimeUtil.DAY))
-            mList.addAll(yesterdayList)
-            val todayList = requester.request(TimeUtil.getDayStr(mNewsLastTime))
-            mList.addAll(todayList)
-            mNewsLastTime -= 2 * TimeUtil.DAY
-            Handler(Looper.getMainLooper()).post {
-                activity.onLoadFirstFinish(mList)
-                mIsRefreshing = false
+        GlobalScope.launch(Dispatchers.Main) {
+            if (mIsRefreshing) {
+                return@launch
             }
-        }.start()
+            if (mIsLoadingMore) {
+                mIsLoadingMore = false
+            }
+            mIsRefreshing = true
+            mNewsLastTime = System.currentTimeMillis()
+            val list = withContext(Dispatchers.IO) {
+                requestTwoDayNews()
+            }
+            mNewsLastTime -= 2 * TimeUtil.DAY
+            mList.clear()
+            mList.addAll(list)
+            activity.onLoadFirstFinish(mList)
+            mIsRefreshing = false
+        }
     }
 
     fun loadMore() {
-        if (mIsRefreshing) {
-            return
-        }
-        if (mIsLoadingMore) {
-            return
-        }
-        mIsLoadingMore = true
-        Thread{
-            val requester = Requester()
-            val yesterdayList = requester.request(TimeUtil.getDayStr(mNewsLastTime - TimeUtil.DAY))
-            mList.addAll(yesterdayList)
-            val todayList = requester.request(TimeUtil.getDayStr(mNewsLastTime))
-            mList.addAll(todayList)
-            mNewsLastTime -= 2 * TimeUtil.DAY
-            Handler(Looper.getMainLooper()).post {
-                if (mIsRefreshing
-                        || !mIsLoadingMore) {
-                    return@post
-                }
-                activity.onLoadMoreFinish(mList)
-                mIsLoadingMore = false
-
+        GlobalScope.launch(Dispatchers.Main) {
+            if (mIsRefreshing) {
+                return@launch
             }
-        }.start()
+            if (mIsLoadingMore) {
+                return@launch
+            }
+            mIsLoadingMore = true
+            val list = withContext(Dispatchers.IO) {
+                requestTwoDayNews()
+            }
+            if (mIsRefreshing
+                || !mIsLoadingMore) {
+                return@launch
+            }
+            mList.addAll(list)
+            activity.onLoadMoreFinish(mList)
+            mIsLoadingMore = false
+        }
+    }
+
+    private fun requestTwoDayNews(): List<News> {
+        val requester = Requester()
+        val yesterdayList = requester.request(TimeUtil.getDayStr(mNewsLastTime - TimeUtil.DAY))
+        val list = ArrayList<News>()
+        list.addAll(yesterdayList)
+        val todayList = requester.request(TimeUtil.getDayStr(mNewsLastTime))
+        list.addAll(todayList)
+        return list
     }
 }
